f1bb02097e751d9879a3fea6bf48c182e47448b0
Ankita Deb <ankitadeb1202@gmail.com>
2023-03-01 19:02:14 +0530

    added

--- Models/Job.model.js
+++ Models/Job.model.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 const jobSchema=mongoose.Schema({
     title:String,
-    posting_date:Date,
+    posting_date:String,
     location:String,
     description:String,
     type:String,
